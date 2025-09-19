69a2dd93c70a8b5011dbab399e8a7c5f98f05439
Yunus Kapdawala <yunuszakir830@gmail.com>
2025-09-19 19:05:05 +0530

    s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'  // Local NestJS server
+  apiUrl: 'https://task-management-backend-production-23cd.up.railway.app'  // Local NestJS server
 };
